956f6cb37e95a6f4ed1f535830197a9d391459b0
ncolyer <emeraldcreeper7@outlook.com>
2024-11-06 16:56:34 +0000

    updated stemlight to v1.1.11-beta and fixed some discord msg change where you need to now do message.content.length instead of just message.length, you do you man you do you

--- src/wdl-bot.ts
+++ src/wdl-bot.ts
@@ -126,7 +126,8 @@ client.on('messageDelete', (message) => {
   if (testingChannel) {
     // If any mention found, replace them with text to avoid ping
     const sanitizedContent = message.content.replace(/@everyone/g, '`@everyone`').replace(/@here/g, '`@here`');
-    if (message.length < 1900) {
+    console.log(message)
+    if (message.content.length < 1900) {
       testingChannel.send(`**Deleted Message:** \n- User: *${message.author.username}*\n- Channel: ${message.channel}\n- Message: "${sanitizedContent}"`);
     }
     console.log('Message deleted');
